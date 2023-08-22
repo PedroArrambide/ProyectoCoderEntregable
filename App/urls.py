c23e7a2295dc0f8204552f55b883dba9878e1918
Pedro Arrambide <p.arramabide@e-fmc.com.ar>
2023-08-21 23:47:36 -0300

    Segunda Entrega

--- App/urls.py
+++ App/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('probandoTemplates/',views.probandoTemplates,name= 'probandoTemplates'),
     path('curso_list/', views.curso_list, name='curso_list'),
     path('curso_create/', views.curso_create, name='curso_create'),
+    path('index/', views.index, name='index'),
     ]
 
--- App/views.py
+++ App/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render, redirect
+from django.template import Template, Context
+from django.http import HttpResponse
 from .models import Curso
 
-
-def probandoTemplates(request):
-    # No es recomendable cargar una plantilla de esta manera en Django.
-    # En su lugar, usa el sistema de plantillas incorporado.
-    return render(request, 'templates1.html')
+# Create your views here.
+def probandoTemplates(self): 
+    mi_html = open ("C:/Users/Pedro M. Arrambide/AppData/Local/Packages/PythonSoftwareFoundation.Python.3.11_qbz5n2kfra8p0/LocalCache/local-packages/Python311/Scripts/ProyectoCoderEntregable/App/Templates/templates1.html")
+    plantilla = Template(mi_html.read())
+    mi_html.close()
+    miContexto = Context()
+    documento = plantilla.render(miContexto)
+    return HttpResponse(documento)
 
 def curso_list(request):
     curso = Curso.objects.all()
-    return render(request, 'curso_list.html', {'curso': curso})
+    return render(request,'curso_list.html', {'curso': curso})
 
 def curso_create(request):
     if request.method == 'POST':
@@ -17,5 +22,8 @@ def curso_create(request):
         numero_comision = request.POST.get('numero_comision')
         curso = Curso(nombre=nombre, numero_comision=numero_comision)
         curso.save()
-        return redirect('curso_list')
+        return redirect('curso_list.html')
     return render(request, 'curso_create.html')
+
+def index(request):
+    return render(request, 'index.html')
