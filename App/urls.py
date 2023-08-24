7094e8622f7bcbed4d91fa123066d1bf5a830d2f
Pedro Arrambide <p.arramabide@e-fmc.com.ar>
2023-08-23 23:01:56 -0300

    tercera entrega2

--- App/urls.py
+++ App/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('probandoTemplates/',views.probandoTemplates,name= 'probandoTemplates'),
+    path('probandoTemplates/', views.probandoTemplates, name='probandoTemplates'),
     path('curso_list/', views.curso_list, name='curso_list'),
     path('curso_create/', views.curso_create, name='curso_create'),
     path('index/', views.index, name='index'),
--- App/views.py
+++ App/views.py
@@ -5,7 +5,7 @@ from .models import Curso
 
 # Create your views here.
 def probandoTemplates(self): 
-    mi_html = open ("C:/Users/Pedro M. Arrambide/AppData/Local/Packages/PythonSoftwareFoundation.Python.3.11_qbz5n2kfra8p0/LocalCache/local-packages/Python311/Scripts/ProyectoCoderEntregable/App/Templates/templates1.html")
+    mi_html = open("C:/Users/Pedro M. Arrambide/AppData/Local/Packages/PythonSoftwareFoundation.Python.3.11_qbz5n2kfra8p0/LocalCache/local-packages/Python311/Scripts/ProyectoCoderEntregable/App/Templates/curso_create.html")
     plantilla = Template(mi_html.read())
     mi_html.close()
     miContexto = Context()
@@ -22,7 +22,8 @@ def curso_create(request):
         numero_comision = request.POST.get('numero_comision')
         curso = Curso(nombre=nombre, numero_comision=numero_comision)
         curso.save()
-        return redirect('curso_list.html')
+        return redirect('App:curso_list')
+    
     return render(request, 'curso_create.html')
 
 def index(request):
